ee50594c65948abe247a391fd57df6262d44a049
Aymen Al-Alimi <a7yan95@gmail.com>
2025-08-05 14:16:24 -0700

    Create app_sizes.dart

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,7 @@
+/// Layout constants used for padding, margins, and other sizing values.
+class AppSizes {
+  static const double padding = 16.0;
+  static const double margin = 16.0;
+  static const double radius = 12.0;
+  static const double iconSize = 24.0;
+}
