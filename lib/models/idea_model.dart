be8f8082c09122ca2b693d284ea76c383e2f7cd6
Aymen Al-Alimi <a7yan95@gmail.com>
2025-08-05 14:26:28 -0700

    Create idea_model.dart

---
+++ lib/models/idea_model.dart
@@ -0,0 +1,12 @@
+/// A model class representing an idea or future project.
+class Idea {
+  final String id;
+  final String title;
+  final String description;
+
+  Idea({
+    required this.id,
+    required this.title,
+    required this.description,
+  });
+}
